9ffe48eec98dc86f19ea66b13505c7fd960b59cf
ankitpandeey <109333871+ankitpandeey@users.noreply.github.com>
2026-01-21 22:23:33 +0530

    project restructuring

--- main.py
+++ main.py
@@ -1,66 +1,35 @@
-import requests
-import json
-import pandas as pd
-from datetime import datetime, timezone
-import time
-from requests.adapters import HTTPAdapter
-from urllib3.util.retry import Retry
 
-# Load cities
-with open(r"C:\Users\MC823AX\OneDrive - EY\Documents\Power BI Learning\Weather App\mp_only.json", "r", encoding="utf-8") as f:
-    cities = json.load(f)
+import pyodbc
 
-url = "https://api.openweathermap.org/data/2.5/weather"
+DRIVER="{ODBC Driver 18 for SQL Server}"
+SERVER="tcp:weatherappp.database.windows.net"
+DATABASE="free-sql-db-9236210;"
+UID= "localhost"
+PWD="A@p8103101921"
 
-rows = []
+conn = pyodbc.connect(f'driver={DRIVER};SERVER={SERVER};DATABASE={DATABASE};UID={UID};PWD={PWD}')
 
-current_time = datetime.now(timezone.utc).isoformat()
-session = requests.Session()
 
-retry_strategy = Retry(
-    total=5,
-    backoff_factor=1,  # 1s, 2s, 4s, 8s, ...
-    status_forcelist=[429, 500, 502, 503, 504],
-    allowed_methods=["GET"]
+cursor = conn.cursor()
+
+cursor.execute("""
+
+CREATE TABLE weather_data (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    city NVARCHAR(100),
+    temperature FLOAT,
+    temperature_min FLOAT,
+    temperature_max FLOAT,
+    humidity INT,
+    sunrise DATETIME,
+    sunset DATETIME,
+    wind_speed FLOAT,
+    recorded_at DATETIME
 )
+""")
 
-adapter = HTTPAdapter(max_retries=retry_strategy)
-session.mount("https://", adapter)
+conn.commit()
+print("Table created!")
 
-for city in cities:
-    params = {
-        "lat": float(city['lat']),
-        "lon": float(city['lon']),
-        "appid": "487ae2216ffdc9fd729cab338dbf483f",
-        "units": "metric" }
-    try:
-        response = session.get(
-            url,
-            params=params,
-            verify=r"C:\Users\MC823AX\ZscalerRootCertificate-2048-SHA256-Feb2025 (2).pem",
-            timeout=20
-        )
-        data = response.json()
-    except Exception as e:
-       print(f"Failed for city {city}: {e}")
-       continue
-
-    row = {
-        "city": data["name"],
-        "lat": data["coord"]["lat"],
-        "lon": data["coord"]["lon"],
-        "weather": data["weather"][0]["description"],
-        "temp": data["main"]["temp"],
-        "temp_min": data["main"]["temp_min"],
-        "temp_max": data["main"]["temp_max"],
-        "humidity": data["main"]["humidity"],
-        "wind_speed": data["wind"]["speed"],
-        "recorded_at": current_time
-    }
-
-    rows.append(row)
-
-# This is what Power BI will import
-df = pd.DataFrame(rows)
-
-df
+cursor.close()
+conn.close()
