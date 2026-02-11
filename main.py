36d4d269734087756605e4663ef9468c32b791db
ankitpandeey <109333871+ankitpandeey@users.noreply.github.com>
2026-02-11 10:40:39 +0530

    aa

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from currentWeath import run_current_weather_pipeline
 from weatherForcast import forecasted_weather_pipeline
+from hourlyWeather import run_hourly_weather_pipeline
 
 def run_pipeline():
-    #run_current_weather_pipeline()
+    run_current_weather_pipeline()
     forecasted_weather_pipeline()
+    run_hourly_weather_pipeline()
+    
 
 
 if __name__ == "__main__":
